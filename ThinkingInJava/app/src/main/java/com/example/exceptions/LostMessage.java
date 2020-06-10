3b8d61014559e7d54bc408fc1d3d4ee9ee06fcbc
曹娅丽 <caoyali@koolearn.com>
2020-06-10 20:58:00 +0800

    提交337页的代码

---
+++ ThinkingInJava/app/src/main/java/com/example/exceptions/LostMessage.java
@@ -0,0 +1,36 @@
+package com.example.exceptions;
+
+class VeryImportantException extends Exception {
+    @Override
+    public String toString() {
+        return "A very important exception!";
+    }
+}
+
+class HoHumException extends Exception {
+
+}
+public class LostMessage {
+    void f() throws VeryImportantException {
+        throw new VeryImportantException();
+    }
+
+    void dispose() throws HoHumException {
+        throw new HoHumException();
+    }
+
+    public static void main(String[] args) {
+        try {
+          LostMessage im = new LostMessage();
+          try {
+              im.f();
+          } finally {
+              // 注意这里，如果外层嵌套的话，不进行catch，在语法上也是可以的！但是保不齐会出问题！
+              // 比如在此种语句下，VeryImportantException是会被丢掉的！当然，以我的习惯，我可能不会写出这样的代码哈哈哈哈！
+              im.dispose();
+          }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
