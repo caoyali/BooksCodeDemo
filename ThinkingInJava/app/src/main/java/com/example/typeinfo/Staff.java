1db535df82e42782191f005203a346307a79fd8a
曹娅丽 <caoyali@koolearn.com>
2020-07-09 19:59:35 +0800

    提交新的代码

---
+++ ThinkingInJava/app/src/main/java/com/example/typeinfo/Staff.java
@@ -0,0 +1,34 @@
+package com.example.typeinfo;
+
+import java.util.ArrayList;
+
+public class Staff extends ArrayList<Position> {
+    public void add(String title, Person person) {
+        add(new Position(title, person));
+    }
+
+    public Staff(String... titles) {
+        for (String t : titles) {
+            add(new Position(t));
+        }
+    }
+
+    public boolean positionAvaliable(String title) {
+        for (Position position : this) {
+            if (title.equals(position.getTitle())) {
+                return position.getPerson() != Person.NULL;
+            }
+        }
+        return false;
+    }
+
+    public void fillPosition(String title, Person hire) {
+        for (Position position : this) {
+            if (title.equals(position.getTitle()) && Person.NULL == position.getPerson()) {
+                position.setPerson(hire);
+            }
+        }
+    }
+
+
+}
