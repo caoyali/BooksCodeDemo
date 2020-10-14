6d385cf5ad028988262d55c4937434fd938afbad
曹娅丽 <caoyali@koolearn.com>
2020-10-14 14:11:49 +0800

    [feature] 抽象工厂模式--> 添加一些抽象工厂模式的注释

--- 23Pattern/app/src/main/java/com/example/pattern/AbstractFactroy/FactroyFactroy.java
+++ 23Pattern/app/src/main/java/com/example/pattern/AbstractFactroy/FactroyFactroy.java
@@ -1,5 +1,9 @@
 package com.example.pattern.AbstractFactroy;
 
+/**
+ * 抽象工厂模式
+ * 是基于工厂模式之上的一个工厂模式，在我看来很适合进行组装搭配类需求的实现。
+ */
 public class FactroyFactroy{
 	public static final String NAME_SHAPE = "shape";
 	public static final String NAME_COLOR = "color";
