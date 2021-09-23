b1b3969942488dc592ae7822d9abaa78eb1eb8c4
caoyali <caoyali2579@163.com>
2021-09-23 09:46:11 +0800

    [note] shell--> 新增shell的学习笔记

---
+++ shellscript/test.sh
@@ -0,0 +1,8 @@
+if [ ! -d "/Users/admin/my_github/BooksCodeDemo/aaaaa" ];then
+    echo "目录不存在"
+    mkdir /Users/admin/my_github/BooksCodeDemo/aaaaa
+    mkdir /Users/admin/my_github/BooksCodeDemo/aaaaa/bbb
+    mkdir /Users/admin/my_github/BooksCodeDemo/aaaaa/bbb
+    mkdir /Users/admin/my_github/BooksCodeDemo/aaaaa/bbb
+fi
+    
