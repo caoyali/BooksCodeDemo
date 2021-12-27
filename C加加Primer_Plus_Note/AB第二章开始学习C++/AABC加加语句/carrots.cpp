b3f7ea7b5a5f9b4be8ae44c33c0c21f9b541ab30
caoyali <caotali2579@163.com>
2021-12-27 22:09:27 +0800

    [c++]提交C++的学习笔记

--- C加加Primer_Plus_Note/AB第二章开始学习C++/AABC加加语句/carrots.cpp
+++ C加加Primer_Plus_Note/AB第二章开始学习C++/AABC加加语句/carrots.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-int main
+using namespace std;
+int main()
 {
-    using namespace std;
     int carrots;
     carrots = 25;
     cout << "I have";
     cout << carrots;
-    cout << " carrots."
+    cout << " carrots.";
     cout << endl;
 
     carrots -= 1;
