1ba5a9e10066291b113b2f7c96f98c71c77248d6
caoyali@kanzhun.com <caoyali@kanzhun.com>
2022-07-25 17:40:09 +0800

    [c++]--> 提交C+学习笔记

---
+++ C加加Primer_Plus_Note/F分支语句和逻辑运算符/A格式化ifelse语句/DFillArray.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+int fillArray(double aar[], int limit);
+int main() {
+    double aar[] = new aar[5];
+    fillArray(aar, 5);
+    return 0;
+}
+
+int fillArray(double aar[], int limit) {
+    int i;
+    using namespace std;
+    double temp;
+
+    for (i = 0; i < limit; i++) {
+        cout << "Enter value #" << ()
+    }
+}
---
+++ C加加Primer_Plus_Note/F分支语句和逻辑运算符/A格式化ifelse语句/EAARFun3.cpp
@@ -0,0 +1,3 @@
+#include<iostream>
+int fill_array(double ar[], int limit);
+void show_array(const double ar[], int n);
